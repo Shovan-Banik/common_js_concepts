4d05d26866e8c8f0f50111f4a283802ed7e1ece1
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-02-28 04:02:43 +0600

    difference between double equal and triple equal

---
+++ tripple.js
@@ -0,0 +1,17 @@
+const first =0;
+const second=false;
+if(first==second){
+    console.log('values are equal')
+}else{
+    console.log('values are not equal')
+}
+// aikhane equal asbe....
+
+const a={x:5};
+const b={x:5};
+if(a===b){
+    console.log('values are equal')
+}else{
+    console.log('values are not equal')
+}
+// aikhane not equal asbe 
