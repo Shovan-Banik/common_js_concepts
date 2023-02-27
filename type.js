6449d97b6fedd71d5e0914ece78ad16a1d29acad
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-02-28 01:35:05 +0600

    type of js capturing

---
+++ type.js
@@ -0,0 +1,12 @@
+// java script is a dynamic type programming language
+
+let p=10;
+let q=p;
+q=15;
+console.log(p,q);/* premitive data type bole sudhu q ar man change hbe....aitai sadharonoto hoy */
+
+let a={id:1,name:'pk'};
+let b=a;
+b.id=2;
+b.name='dk';
+console.log(a,b)/* aikhane chng krci sudhu b ar property kinto a abong b 2 tar..e proerty change hye jbe...karon aigula non-primitive type...aigula tar memory ar reference dhore rakhe....tai nton value asign krle referce onojayi 2 tai chang hoy */
